d6ce396b2dec9e61191c43de31c137a4fba3e37c
luis aaron <aaron32158@gmail.com>
2023-08-10 00:28:04 -0600

    edit smeakers

--- middleware/isSneakerOwner.js
+++ middleware/isSneakerOwner.js
@@ -1,20 +1,22 @@
-
 const Sneaker = require('../models/Sneaker');
 
-const isSneakerOwner = () => {
-
-    Sneaker.findById(req.params.id)
-        .then((foundSneaker) => {
-            if (req.body.owner === foundSneaker.owner.toString()) {
-                next()
-            } else {
-                res.status(401).json({message: "Validation Error"})
-            }
-        })
-        .catch((err) => {
-            console.log(err)
-            next(err)
-        })
-}
+const isSneakerOwner = (req, res, next) => {
+  Sneaker.findById(req.params.sneakerId)
+    .then((foundSneaker) => {
+      if (!foundSneaker) {
+        return res.status(404).json({ message: "Sneaker not found" });
+      }
+      
+      if (req.body.owner === foundSneaker.owner.toString()) {
+        next();
+      } else {
+        res.status(401).json({ message: "Validation Error" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
+};
 
 module.exports = isSneakerOwner;
